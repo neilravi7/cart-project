20fe22199583979d5bb25b9a10bfe2576ea68369
Ravi Bagdi <63995407+neilravi7@users.noreply.github.com>
2024-08-18 09:05:21 +0530

    Update firebase.js
    Fix misspelled

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDd-dM4QyEeUUykk-Z77DW8Jixz5lu1ZCo",
-    authDomain: "cart-9b2a6.firebaseapp.com",
-    projectId: "cart-9b2a6",
-    storageBucket: "cart-9b2a6.appspot.com",
-    messagingSenderId: "800527305336",
-    appId: "1:800527305336:web:7e0ef3a447b40e27467b8d"
+    apiKey: "XXXX",
+    authDomain: "XXXX",
+    projectId: "XXXX",
+    storageBucket: "XXXXXXX",
+    messagingSenderId: "XXXXXX",
+    appId: "XXXXXXXXXXXX"
   };
 
 // Initialize Firebase
